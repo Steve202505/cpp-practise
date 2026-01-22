befcd6c62789dbf20d5d6dc9751f4813170d9db7
Steve202505 <oceantaste12@gmail.com>
2026-01-22 13:19:28 +0000

    added

--- 01_Basics/one.cpp
+++ 01_Basics/one.cpp
@@ -3,5 +3,9 @@
 using namespace std;
 
 int main(){
+    int num1, num2;
     cout << "Hello World!";
+    cin >> num1 >> num2;
+    cout << "Number1: %d and Number2: %d", num1, num2;
+    return 0;
 }
