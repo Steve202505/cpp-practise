1ff2850d7ba4a741d40b20e8ba999b093faec7a2
Steve202505 <oceantaste12@gmail.com>
2026-01-22 13:30:24 +0000

    add

--- 01_Basics/one.cpp
+++ 01_Basics/one.cpp
@@ -4,8 +4,8 @@ using namespace std;
 
 int main(){
     int num1, num2;
-    cout << "Hello World!";
+    cout << "Hello World!" << endl;
     cin >> num1 >> num2;
-    cout << "Number1: %d and Number2: %d", num1, num2;
+    cout << "Number1: " << num1 << " and Number2: " << num2;
     return 0;
 }
